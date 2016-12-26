c13e844e7f160a9e1ff79dd345e19698d9bd55b0
4O4 <pk.pawelo@gmail.com>
2016-12-26 18:38:58 +0100

    Fix recursive table copying

--- shared/lua_extensions.lua
+++ shared/lua_extensions.lua
@@ -3,7 +3,7 @@ function table.copy(theTable)
 
 	for key, value in pairs(theTable) do
 		if type(value) == "table" then
-			newTable[key] = table.copy(theTable)
+			newTable[key] = table.copy(value)
 		else
 			newTable[key] = value
 		end
